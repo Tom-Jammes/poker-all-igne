3e9f9c1812f20dc9716ddf4f467717f80b7b9671
tom-jammes <tom.jammes2@gmail.com>
2025-05-04 09:51:27 -0400

    Création des vues

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def page_accueil():
+    return render_template("index.html")
 
+@app.route('/create')
+def page_creer():
+    return render_template("creerTable.html")
+
+@app.route('/join')
+def page_rejoindre():
+    return render_template("rejoindreTable.html")
+
+@app.route('/salle-attente')
+def page_salle_attente():
+    return render_template("salleAttente.html")
 
 if __name__ == '__main__':
     app.run()
